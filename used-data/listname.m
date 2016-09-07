36223ac4090f709e199c724eb082cbf347330a3e
szhfei <351404276@qq.com>
2016-09-07 16:46:02 +0800

    modify listname resave_data

--- used-data/listname.m
+++ used-data/listname.m
@@ -1,36 +1,41 @@
-function [filename1,filename2]=listname(x1,x2)   %从小到大，同时排序未转存的两种文件
+function [filename1,filename2]=listname(x1,x2)   %从小到大，排序未转存的两种文件
 file1=dir([x1,'*.t3w']);
 file2=dir([x2,'*.mat']);
 l1=length(file1);
 l2=length(file2);
-% if(l1/l2~=3)
-%     disp('t3w and mat numbers unfit');
-%     return
-% end
-namelength1=length(file1(1).name);
-namelength2=length(file2(1).name);
-for i=1:l1
-    filename1{i}=file1(i).name;
-    for j=i+1:l1
-        if(str2num(file1(i).name((namelength1-17):(namelength1-8)))>str2num(file1(j).name((namelength1-17):(namelength1-8))))%月日时分秒
-            filename1{i}=file1(j).name;
-            file1(j).name=file1(i).name;
-            file1(i).name=filename1{i};
+
+if(l1 == 0)
+    filename1 = false;
+else
+    namelength1=length(file1(1).name);
+    for i=1:l1
+        filename1{i}=file1(i).name;
+        for j=i+1:l1
+            if(str2num(file1(i).name((namelength1-17):(namelength1-8)))>str2num(file1(j).name((namelength1-17):(namelength1-8))))%月日时分秒
+                filename1{i}=file1(j).name;
+                file1(j).name=file1(i).name;
+                file1(i).name=filename1{i};
+            end
         end
+        fname1(i)=str2num(filename1{i}((namelength1-17):(namelength1-8)));
     end
-    fname1(i)=str2num(filename1{i}((namelength1-17):(namelength1-8)));
 end
-for i=1:l2
-    filename2{i}=file2(i).name;
-    for j=i+1:l2
-        if(str2num(file2(i).name((namelength2-13):(namelength2-4)))>str2num(file2(j).name((namelength2-13):(namelength2-4))))%月日时分秒
-            filename2{i}=file2(j).name;
-            file2(j).name=file2(i).name;
-            file2(i).name=filename2{i};
+if(l2 == 0)
+    filename2 = false;
+else
+    namelength2=length(file2(1).name);
+    for i=1:l2
+        filename2{i}=file2(i).name;
+        for j=i+1:l2
+            if(str2num(file2(i).name((namelength2-13):(namelength2-4)))>str2num(file2(j).name((namelength2-13):(namelength2-4))))%月日时分秒
+                filename2{i}=file2(j).name;
+                file2(j).name=file2(i).name;
+                file2(i).name=filename2{i};
+            end
         end
+        temp=changename(filename2{i});
+        fname2(i)=str2num(temp((namelength2-13):(namelength2-4)));
     end
-    temp=changename(filename2{i});
-    fname2(i)=str2num(temp((namelength2-13):(namelength2-4)));
 end
 clear temp
 
--- used-data/resave_data.m
+++ used-data/resave_data.m
@@ -1,13 +1,14 @@
+clear all
 [filename1,filename2]=listname('D:\szh\test\used-data\','D:\szh\test\used-data\');
-l1=length(filename1);
-l2=length(filename2);
 
+l1=length(filename1);
 % for i=1:l1
 %     fname1=filename1{i};
 %     resave(fname1);
 % end
 
-for i=1:l2
-    fname2=filename2{i};
-    resave(fname2);
-end
+% l2=length(filename2);
+% for i=1:l2
+%     fname2=filename2{i};
+%     resave(fname2);
+% end
